d39f36b07444c939e278cd00fa869d5d77211d23
ksha23 <74990237+ksha23@users.noreply.github.com>
2022-10-02 03:55:56 -0500

    Update EPA04.java

--- EPA04.java
+++ EPA04.java
@@ -1,3 +1,29 @@
+/*
+EPA04: Space Station Repair Lights
+Description/Instructions:
+You’ve been hired to work on a systems diagnostic unit for the International Space Station.  This unit will monitor four critical systems – a Battery, a Heat Exchanger, a Water Pump, and a Temperature Sensor – to determine if they are working or not.  However, space is at a premium on the ISS, and so you will only have room for two LED lights for the astronauts to see!
+
+Your team has come up with a design that will allow the astronauts to determine which systems, if any, are broken based on the colors of the two LED lights.  Each system you’re monitoring is given a numerical value based on how critical it is to the operation and safety of the station:
+Battery: 8
+Heat Exchanger: 4
+Water Pump: 2
+Temperature Sensor: 1
+
+Whenever the unit runs, it will add the values of any broken systems together and light up the LEDs to indicate the resulting number.  Each LED has four states, each representing a number – off (0), red (1), green (2), and blue (3).  The astronauts will multiply the left LED’s value by four, then add it to the right LED’s value, in order to determine the correct error code.  For example, if the left LED is red and the right LED is green, the astronauts would calculate the error code as (1 * 4) + 2 = 6.
+
+HINT: You should use modulo division for this assignment to compute the error code lights.
+
+Your task is to write the logic that controls the LEDs.
+
+Input:
+You must read a file titled data.txt will contain one word on each line denoting a part’s status as WORKING or BROKEN in all caps with no spaces.
+
+Output:
+Your program should output the length of each sequence in the following format:
+leftLightStatus rightLightStatus
+
+*/
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
